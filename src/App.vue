3b56776347628d65f26740d33010cf2967e8ecc1
pcorrell <pcorrell@vt.edu>
2019-06-04 13:44:21 -0400

    paul-test: got a header component working

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <app-navigation></app-navigation>
+    <!-- <app-navigation></app-navigation> -->
+    <app-header></app-header>
     <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -10,10 +11,12 @@
 </template>
 
 <script>
-import Navigation from './components/Navigation'
+import Navigation from './components/Navigation';
+import Header from './components/Header';
 export default {
   components: {
-    'app-navigation': Navigation
+    'app-navigation': Navigation,
+    'app-header': Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+    <!-- Header -->
+    <header id="js-header" class="u-header u-header--sticky-top u-header--change-appearance g-z-index-9999" data-header-fix-moment="100">
+    <div class="u-header__section g-bg-white g-transition-0_3 g-py-6 g-py-18--md" data-header-fix-moment-exclude="g-py-18--md" data-header-fix-moment-classes="u-shadow-v18 g-py-13--md">
+        <nav class="navbar navbar-expand-lg g-py-0">
+        <div class="container-fluid justify-content-between g-pos-rel">
+            <!-- Logo -->
+            <a href="#body" class="js-go-to navbar-brand u-header__logo" data-type="static">
+            <img class="u-header__logo-img u-header__logo-img--main g-width-110" src="../../public/assets/custom/img/case/logo-banner.gif" alt="Case Consulting, Inc.">
+            </a>
+            <!-- End Logo -->
+
+            <!-- Navigation -->
+            <div class="collapse navbar-collapse align-items-center flex-sm-row" id="navBar">
+            <ul id="js-scroll-nav" class="navbar-nav text-uppercase g-font-weight-700 g-font-size-11 g-pt-20 g-pt-5--lg ml-auto">
+                <li class="nav-item g-mr-15--lg g-mb-7 g-mb-0--lg active">
+                <a href="#about" class="nav-link p-0">About</a>
+                </li>
+                <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                <a href="#benefits" class="nav-link p-0">Benefits</a>
+                </li>
+                <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                <a href="#careers" class="nav-link p-0">Careers</a>
+                </li>
+                <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                <a href="#intern" class="nav-link p-0">Internship</a
+                    </li>
+                <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                <a href="#numbers" class="nav-link p-0">Numbers</a>
+                </li>
+                <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                <a href="#awards" class="nav-link p-0">Awards</a>
+                </li>
+                <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                <a href="#events" class="nav-link p-0">Events</a>
+                </li>
+                <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                <a href="#contact" class="nav-link p-0">Contact</a>
+                </li>
+            </ul>
+            </div>
+            <div class="bptw">
+            <a class="navbar-brand u-header__log">
+                <img src="../../public/assets/custom/img/awards/2018_BPTW_logo_C1.png" alt="Best places to work 2018 logo" class ="nav-pad">
+                </a>
+            </div>
+            <!-- End Navigation -->
+            <!-- Responsive Toggle Button --><button class="navbar-toggler btn g-line-height-1 g-brd-none g-pa-0 g-pos-abs g-top-20 g-right-0" type="button" aria-label="Toggle navigation" aria-expanded="false" aria-controls="navBar" data-toggle="collapse" data-target="#navBar">
+                <span class="hamburger hamburger--slider">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+                </span>
+            </button>
+            <!-- End Responsive Toggle Button -->
+        </div>
+        </nav>
+    </div>
+    </header>
+    <!-- End Header -->
+</template>
+
