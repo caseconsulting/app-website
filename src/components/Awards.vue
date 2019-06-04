cdba23504aa6f04f5c7ffe2a99b6bbee03510e30
Sneha Kuchipudi <skuchipudi@consultwithcase.com>
2019-06-04 15:15:55 -0400

    438-awards-component: created Awards.vue file and edited index.html to remove awards code

---
+++ src/components/Awards.vue
@@ -0,0 +1,46 @@
+<template>
+          <!-- Awards Section Content -->
+      <section id="awards" class="g-theme-bg-color-1 g-text-color-2  g-pt-10 g-pb-10">
+        <div><br/><br/>
+          <div class="u-heading-v2-2--bottom text-center g-brd-primary g-mb-20">
+            <h2 class="text-uppercase text-center u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md g-color-white">Awards</h2>
+          </div> <br/>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="container-fluid award-container">
+                  <div class="row award-row">
+                    <div class="col-md-6">
+                      <a href="https://www.bizjournals.com/washington/news/2017/05/05/these-are-the-best-places-to-work-in-greater.html" target="_blank" rel="noopener" border="0">
+                          <img src="../../public/assets/custom/img/awards/2017_BPTW_button_HONOREE.PNG" alt="" class="rounded-circle img-fluid mobile-image award-shadow"/>
+                        </a>
+                    </div>
+                    <div class="col-md-6 award-overlapped">
+                      <a href="https://www.bizjournals.com/washington/news/2018/04/13/see-the-best-places-to-work-in-greater-washington.html" target="_blank" rel="noopener" border="0">
+                          <img src="../../public/assets/custom/img/awards/2018_BPTW_button_HONOREE.PNG" alt="" class="rounded-circle img-fluid mobile-image award-shadow"/>
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <p class="g-color-white">
+                  The Washington Business Journal's Best Places to Work program honors Greater Washington area companies that score highest in annual employee engagement. For the 2nd year in a row, Case Consulting Inc. has been recognized as one of the best places to work
+                  among small companies:
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15"> <i class="fas fa-angle-right bullet_top  g-left-0 g-font-size-16 g-color-white"></i>
+                      <p class="list_elem g-color-white">#3 Small Company in 2018 </p>
+                    </li>
+                    <li class="g-pos-rel g-pl-10 g-mb-15"> <i class="fas fa-angle-right bullet_top  g-left-0 g-font-size-16 g-color-white"></i>
+                      <p class="list_elem g-color-white">#6 Small Company in 2017</p>
+                    </li>
+                  </ul>
+                </p>
+              </div>
+            </div>
+          </div><br/><br/>
+        </div>
+      </section>
+      <!-- End Awards Section -->
+</template>
