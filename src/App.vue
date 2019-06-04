d88103bc0fd22f70622c15acaca138115bbce7a4
Sneha Kuchipudi <skuchipudi@consultwithcase.com>
2019-06-04 15:32:26 -0400

    438-awards-component: fix crash

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <app-about></app-about>
     <app-benefits></app-benefits>
     <app-careers></app-careers>
-    <app-awards> </app-awards>
+    
   </div>
 </template>
 
@@ -20,7 +20,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Benefits from './components/Benefits';
 import Careers from './components/Careers';
-import Awards from './components/Awards';
+
 
 export default {
   components: {
@@ -29,7 +29,7 @@ export default {
     'app-about': About,
     'app-benefits': Benefits,
     'app-careers': Careers, 
-    'app-awards': Awards
+    
   }
 }
 </script>
