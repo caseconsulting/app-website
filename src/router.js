4a0bf8c2e3288b1eab87b51586d6bb8ba79a7ad0
Megan Gilbert <megangilbert@MacBook-Pro-4.local>
2019-06-05 13:45:27 -0400

    545-creating-routes: Added routes for Home and Intern page

--- src/router.js
+++ src/router.js
@@ -1,28 +1,30 @@
 import Vue from "vue";
 import Router from "vue-router";
 //import Home from "./views/Home.vue";
-import Internship from "./components/home/Internship";
+import Internship from "./components/intern/Intern.vue";
+import Home from "./components/home/Home.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    // {
-    //   path: "/",
-    //   name: "home",
-    //   component: Home
-    // },
     {
+<<<<<<< HEAD
       path: "/about",
       name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+=======
+      path: "/",
+      name: "home",
+      component: Home
+>>>>>>> 545-creating-routes: Added routes for Home and Intern page
     },
     {
       path: "/intern",
       componenet: Internship
     },
-    {path: "*",redirect: "/"}
+    { path: "*", redirect: "/" }
   ]
 });
