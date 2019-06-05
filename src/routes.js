a07b5c96ca9b6024047b1d0d5477649f1b17b2d4
Megan Gilbert <megangilbert@MacBook-Pro-4.local>
2019-06-05 17:08:23 -0400

    545-creating-routes: Added all of code from intern.html into Intern.vue

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,19 @@ import Intern from "./components/intern/Intern.vue";
 import Home from "./components/home/Home.vue";
 
 export const routes = [
-         {
-           path: "/",
-           name: "home",
-           components: {
-             default: Home
-           }
-         },
-         {
-           path: "/intern",
-           name: "internship",
-           components: {
-             default: Intern
-           }
-         },
-         { path: "*", redirect: "/" }
-       ];
+  {
+    path: "/",
+    name: "home",
+    components: {
+      default: Home
+    }
+  },
+  {
+    path: "/intern",
+    name: "internship",
+    components: {
+      default: Intern
+    }
+  },
+  { path: "*", redirect: "/" }
+];
