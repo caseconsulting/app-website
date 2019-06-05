c9992fd809185dd8ac03b9ba8c06cda0ac5f8df8
Megan Gilbert <megangilbert@MacBook-Pro-4.local>
2019-06-05 12:55:53 -0400

    436-Internship-Component: took out old router for home

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+//import Home from "./views/Home.vue";
 import Internship from "./components/Internship";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: Home
+    // },
     {
       path: "/about",
       name: "about",
