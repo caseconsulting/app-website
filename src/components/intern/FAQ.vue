795d6f5d6eb49f3e36c041fa830c611d60ff5c36
Austin Lam <alam@consultwithcase.com>
2019-06-06 10:01:48 -0400

    458-Internship-FAQ-Component: moved faq section in internship into a faq component

---
+++ src/components/intern/FAQ.vue
@@ -0,0 +1,282 @@
+<template>
+  <!-- FAQ Section Content -->
+  <section id="faq">
+    <div class="container-fluid">
+      <div class="g-pt-100 g-pb-30">
+        <div class="container text-center g-max-width-750 g-mb-20 g-pt-10">
+          <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
+            <h2
+              class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md"
+            >At-A-Glance</h2>
+          </div>
+        </div>
+        <br>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">Who?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">
+                        Are you a Computer Science or Computer Engineering Junior who enjoys learning and
+                        working with current development technologies? Are you interested in experiencing
+                        what it means to be a professional application developer? If yes, then this
+                        internship is for you!
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">What?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">
+                        During your internship at Case Consulting you will be guided by our team of
+                        software professionals and previous interns to apply cutting edge software
+                        development tools and practices to a real software product. Throughout the
+                        program, you will use many software engineering best practices such as
+                        agile/scrum, pair programming, automated testing, and test-driven development.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">When?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">Summer 2019</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">Where?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p
+                        class="list_elem"
+                      >The internship is located at our shared office space in Reston, VA.</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">Why?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">
+                        Work with cutting edge technologies like JavaScript, Node/Express, Vue, Amazon
+                        Web Services, and Bootstrap. Gain professional experience working in a simulated
+                        customer environment using industry best practices on a self-organizing team,
+                        apply for a security clearance, and work for a top notch tech company.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">How?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">Apply today!</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="container text-center g-max-width-750 g-mb-20 g-pt-10">
+              <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
+                <h3
+                  class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md"
+                >Frequently Asked Questions</h3>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">What is a government contractor?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">
+                        Government Contractors are people who work for a private or public company on
+                        contracts setup to deliver solutions to the Government. These companies either
+                        win prime contracts themselves or partner with companies to work as a
+                        subcontractor. As a contractor you will work at a Government facility with
+                        other contractors and Government employees.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3
+                    class="h5 text-uppercase g-font-weight-800"
+                  >Why do I need a security clearance?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">
+                        A security clearance grants you the ability to work on some of the most exciting
+                        projects out there. You will work to solve very complicated and unique problems
+                        helping people around the world. In addition to the exciting work, a clearance
+                        provides you with an asset that improves job security and your demand to the market.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">Why is there a 1 year commitment?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">
+                        At the end of the internship this question will be obsolete because we take pride in
+                        the experience our internship provides and are confidant you will want to work with
+                        us going forward. A commitment to Case Consulting is necessary to submit you for a
+                        U.S. Government security clearance. As a company we invest heavily in our interns
+                        and take the program very seriously. Everyone involved gives a lot of time and
+                        effort, working to ensure the program is not an average internship experience,
+                        but rather true development of the skills required to succeed as a software engineer
+                        in the real world.
+                      </p>
+                      <p class="list_elem">
+                        Before the completion of a successful internship you can expect to receive an offer
+                        for full time employment with Case Consulting, including your minimum starting
+                        salary and benefits. We understand that this is a big commitment, but we believe
+                        you will love our culture as much as we do and will want to stay well past any
+                        specified commitment time period.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 offset-sm-1 g-mb-40">
+              <div class="media">
+                <div class="d-flex g-mt-25 g-mr-40">
+                  <span class="d-block g-color-primary g-font-size-48 g-line-height-1"></span>
+                </div>
+                <div class="media-body align-self-center">
+                  <h3 class="h5 text-uppercase g-font-weight-800">Where is your full-time work?</h3>
+                  <ul class="list-unstyled g-font-size-12 g-mb-30 g-mb-0--md">
+                    <li class="g-pos-rel g-pl-10 g-mb-15">
+                      <i
+                        class="fas fa-angle-right bullet_top g-left-0 g-font-size-16 g-color-primary"
+                      ></i>
+                      <p class="list_elem">
+                        Case Consulting has quite a few projects to place college new hires on when they
+                        start full time. Our goal is to find one that best fits your skill-sets and to
+                        place you on a team with other Case Consulting employees. Most of Case
+                        Consulting’s work is located in the Northern Virginia area to include Tyson’s,
+                        McLean, Bethesda, Reston, Herndon, and Chantilly.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+  </section>
+</template>
