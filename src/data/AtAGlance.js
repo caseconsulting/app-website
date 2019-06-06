46d395a89b3b745e174aa3bebdf337d8f43b2521
Austin Lam <alam@consultwithcase.com>
2019-06-06 14:09:53 -0400

    458-Internship-FAQ-Component: refactored FAQ component and data

---
+++ src/data/AtAGlance.js
@@ -0,0 +1,8 @@
+export default [
+  { question: "Who?", answer: ["Are you a Computer Science or Computer Engineering Junior who enjoys learning and working with current development technologies? Are you interested in experiencing what it means to be a professional application developer? If yes, then this internship is for you!"] },
+  { question: "What?", answer: ["During your internship at Case Consulting you will be guided by our team of software professionals and previous interns to apply cutting edge software development tools and practices to a real software product. Throughout the program, you will use many software engineering best practices such as agile/scrum, pair programming, automated testing, and test-driven development."] },
+  { question: "When?", answer: ["Summer 2019"] },
+  { question: "Where?", answer: ["The internship is located at our shared office space in Reston, VA."] },
+  { question: "Why?", answer: ["Work with cutting edge technologies like JavaScript, Node/Express, Vue, Amazon Web Services, and Bootstrap. Gain professional experience working in a simulated customer environment using industry best practices on a self-organizing team, apply for a security clearance, and work for a top notch tech company."] },
+  { question: "How?", answer: ["Apply today"] }
+];
