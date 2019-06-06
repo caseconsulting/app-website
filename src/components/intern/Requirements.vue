d14d51b335a2b370f677598bca6c32bb53105c05
Zak Ramsis <zramsis@Zaks-MacBook-Pro.local>
2019-06-06 09:43:36 -0400

    452-vueify-intern-requirements: add Requirements.vue with requirements section. Added component in Intern.vue.

---
+++ src/components/intern/Requirements.vue
@@ -0,0 +1,57 @@
+<template>
+    <!-- Requirements Section Content -->
+    <section id="requirements" class="g-theme-bg-color-1 g-text-color-2 no-column-space">
+      <div class="container-fluid">
+        <div class="container text-center g-max-width-750 g-mb-20 g-pt-70">
+          <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
+            <h2
+              class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md g-color-white"
+            >Requirements</h2>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row g-color-white">
+            <div class="col-md-4">
+              <ul>
+                <h3 class="g-color-white">
+                  <strong>Technical Skills</strong>
+                </h3>
+                <li>Minimum 3.0 GPA</li>
+                <li>Majoring in Computer Science, Computer Engineering, or related field</li>
+                <li>Experience with object oriented software development</li>
+                <li>Desire to apply and develop technical skills</li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <ul>
+                <h3 class="g-color-white">
+                  <strong>Collaborative Skills</strong>
+                </h3>
+                <li>Works well in team environment</li>
+                <li>Strong written and communication skills</li>
+                <li>Desire to apply and develop interpersonal skills</li>
+                <li>Likes to have fun</li>
+              </ul>
+            </div>
+            <div class="col-md-4" style="margin-bottom:100px;">
+              <ul>
+                <h3 class="g-color-white">
+                  <strong>Other Requirements</strong>
+                </h3>
+                <li>
+                  Ability to obtain a security clearance
+                  <ul>
+                    <li>Must be a U.S. Citizen</li>
+                    <li>No recreational drug use within the last year</li>
+                  </ul>
+                </li>
+                <li>Desire to work in the Northern Virginia area</li>
+                <li>Currently in Junior year</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Requirements Section Content -->
+</template>
