c40b4fb85f06a90d607512d91aa79ca230b7f0c6
Megan Gilbert <megangilbert@MacBook-Pro-4.local>
2019-06-06 08:23:11 -0400

    545-creating-routes: when going to intern page now takes user to top of page

--- src/main.js
+++ src/main.js
@@ -10,7 +10,18 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: "history"
+  mode: "history",
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        selector: to.hash
+      };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
 Vue.config.productionTip = false;
 
