1f1c32bab1369be7760d6549b0c4403ac944e405
Austin Lam <alam@consultwithcase.com>
2019-06-06 09:32:39 -0400

    455-Internship-Slider-Component: put slider in slider component for intern

--- src/components/intern/Intern.vue
+++ src/components/intern/Intern.vue
@@ -82,46 +82,7 @@
     </header>
 
     <!-- Section Content -->
-    <!-- Section Content -->
-      <section class="rev_slider_wrapper fullwidthbanner-container" data-alias="image-hero20">
-        <!-- START REVOLUTION SLIDER 5.0.7 fullwidth mode -->
-        <div id="promoSlider" class="rev_slider fullwidthabanner" style="display: none;" data-version="5.0.7">
-          <ul>
-            <!-- SLIDE  -->
-            <li style="text-align: center;" data-index="rs-68" data-transition="zoomout" data-slotamount="default" data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut" data-masterspeed="2000" data-thumb="../../../public/assets/img-temp/promo/img1.jpg" data-rotate="0" data-saveperformance="off" data-title="Intro">
-              <img src="../../../public/assets/custom/img/case/intern_edit.jpg" alt="Image description" data-bgposition="50% 10%" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina>
-              <!-- LAYERS -->
-              <!-- LAYER NR. 1 -->
-              <div id="promoSliderLayer10" class="tp-caption tp-shape tp-shapewrapper rs-parallaxlevel-0" style="z-index: 5; background-color: rgba(78, 67, 83, .4); border-color: rgba(78, 67, 83, .5);" data-x="['center','center','center','center']" data-y="['middle','middle','middle','middle']"
-                data-hoffset="['0','0','0','0']" data-voffset="['0','0','0','0']" data-width="full" data-height="full" data-whitespace="nowrap" data-transform_idle="o:1;" data-transform_in="opacity:0;s:1500;e:Power3.easeInOut;" data-transform_out="s:300;s:300;"
-                data-start="750" data-basealign="slide" data-responsive_offset="on" data-responsive="off">
-              </div>
-              <!-- LAYER NR. 3 -->
-              <div id="promoSliderLayer1" class="tp-caption NotGeneric-Title tp-resizeme rs-parallaxlevel-0" style="z-index: 7; white-space: nowrap; text-align: center; text-transform: uppercase;" data-x="['center','center','center','center']" data-y="['middle','middle','middle','middle']"
-                data-hoffset="['0','0','0','0']" data-voffset="['-60','-60','-22','-29']" data-fontsize="['60','60','60','40']" data-lineheight="['64','64','64','44']" data-width="none" data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
-                data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;" data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:0px;" data-mask_out="x:inherit;y:inherit;"
-                data-start="1000" data-splitin="none" data-splitout="none" data-responsive_offset="on">
-                Summer Internship
-              </div>
-              <!-- LAYER NR. 4 -->
-              <div id="promoSliderLayer4" class="tp-caption NotGeneric-SubTitle tp-resizeme rs-parallaxlevel-0" style="z-index: 8; white-space: nowrap; text-align: center; text-transform: uppercase;" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                data-y="['middle','middle','middle','middle']" data-voffset="['52','52','28','13']" data-visibility="['on','off']" data-width="none" data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;" data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:1500;e:Power4.easeInOut;"
-                data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;" data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000" data-splitin="none" data-splitout="none"
-                data-responsive_offset="on">
-              </div>
-              <!-- LAYER NR. 5 -->
-              <div id="promoSliderLayer7" class="tp-caption NotGeneric-CallToAction rev-btn rs-parallaxlevel-0" style="z-index: 9; font-weight: bold; white-space: nowrap; outline: none; box-shadow: none; box-sizing: border-box; text-transform: uppercase; border-width: 2px;"
-                data-x="['center','center','center','center']" data-y="['middle','middle','middle','middle']" data-hoffset="['0','0','0','0']" data-voffset="['134','134','80','65']" data-width="none" data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
-                data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;" data-style_hover="c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);cursor:pointer;" data-transform_in="y:50px;opacity:0;s:1500;e:Power4.easeInOut;" data-transform_out="y:[175%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1250" data-splitin="none" data-splitout="none" data-actions='[{"event":"click","action":"scrollbelow","offset":"-90px"}]' data-responsive_offset="on" data-responsive="off">Learn More
-              </div>
-            </li>
-          </ul>
-          <div class="tp-static-layers"></div>
-          <div class="tp-bannertimer tp-bottom" style="visibility: hidden !important;"></div>
-        </div>
-      </section>
-    <!-- END REVOLUTION SLIDER -->
+    <app-slider></app-slider>
     <!-- About Section Content -->
     <section id="overview" class="g-py-20">
       <div class="container text-center g-max-width-750 g-mb-20">
@@ -506,70 +467,85 @@
             >Testimonials</h2>
           </div>
           <div class="container">
-          <div class="row">
-            <div class="col-md-2" style="text-align:center">
-              <img src="https://res.cloudinary.com/consultwithcase/image/upload/v1535720007/website/interns/fahad.jpg" class="rounded-circle potrait" />
+            <div class="row">
+              <div class="col-md-2" style="text-align:center">
+                <img
+                  src="https://res.cloudinary.com/consultwithcase/image/upload/v1535720007/website/interns/fahad.jpg"
+                  class="rounded-circle potrait"
+                >
+              </div>
+              <div class="col-md-10">
+                <p class="g-color-white">
+                  "Case Consulting is an awesome company that has its hands in a lot of interesting technology. At this internship, I learned what it meant to be professional in the field, I learned full-stack technologies such as NodeJS and industry standards such as
+                  OAuth. We also got opportunities to explore the Amazon ecosystem and deployed an application into the real world that will be used by real people. The Case internship is a rewarding opportunity that is centered around great mentors that
+                  will do everything to help you succeed." - Fahad Ashraf, 2017/2018 Intern
+                </p>
+              </div>
             </div>
-            <div class="col-md-10">
-              <p class="g-color-white">
-                "Case Consulting is an awesome company that has its hands in a lot of interesting technology. At this internship, I learned what it meant to be professional in the field, I learned full-stack technologies such as NodeJS and industry standards such as
-                OAuth. We also got opportunities to explore the Amazon ecosystem and deployed an application into the real world that will be used by real people. The Case internship is a rewarding opportunity that is centered around great mentors that
-                will do everything to help you succeed." - Fahad Ashraf, 2017/2018 Intern
-              </p>
+            <div class="row" style="margin-top:100px;">
+              <div class="col-md-10 order-md-1 order-2">
+                <p class="g-color-white">
+                  "The Case internship gave me an amazing insight into what I was getting myself into when I said I wanted to be a software developer. The company is a big family which was a major factor in my decision to come on. I learned real-world programming languages
+                  and used tech that I found interesting every day I was there." - Jacob Boisseau, 2017/2018 Intern
+                </p>
+              </div>
+              <div class="col-md-2 order-md-2" style="text-align:center">
+                <img
+                  src="https://res.cloudinary.com/consultwithcase/image/upload/c_crop,g_custom/c_scale,w_160/v1535719942/website/interns/jacob.jpg"
+                  class="rounded-circle potrait"
+                  style="text-align:center"
+                >
+              </div>
             </div>
-          </div>
-          <div class="row" style="margin-top:100px;">
-            <div class="col-md-10 order-md-1 order-2">
-              <p class="g-color-white">
-                "The Case internship gave me an amazing insight into what I was getting myself into when I said I wanted to be a software developer. The company is a big family which was a major factor in my decision to come on. I learned real-world programming languages
-                and used tech that I found interesting every day I was there." - Jacob Boisseau, 2017/2018 Intern
-              </p>
+            <div class="row" style="margin-top:100px;">
+              <div class="col-md-2" style="text-align:center">
+                <img
+                  src="https://res.cloudinary.com/consultwithcase/image/upload/v1535719942/website/interns/andrew.gif"
+                  class="rounded-circle potrait"
+                >
+              </div>
+              <div class="col-md-10">
+                <p class="g-color-white">
+                  "After completing this internship, I feel as though I went from a programmer who only wanted to stay in the backend to a full stack developer who can switch between the frontend and backend depending on the needs of the team. Through the internship you
+                  will learn how to be an agile developer, while working with cutting edge web technologies such as Vue, Node.js, AWS services, and Express." - Andrew Cooper, 2018 Intern
+                </p>
+              </div>
             </div>
-            <div class="col-md-2 order-md-2" style="text-align:center">
-              <img src="https://res.cloudinary.com/consultwithcase/image/upload/c_crop,g_custom/c_scale,w_160/v1535719942/website/interns/jacob.jpg" class="rounded-circle potrait" style="text-align:center" />
+            <div class="row" style="margin-top:100px;">
+              <div class="col-md-10 order-md-1 order-2">
+                <p class="g-color-white">
+                  "Working at Case Consulting was a great opportunity for me to learn and experience what being a software developer is like in the real world. While I was at my internship, I was able to work in an encouraging, friendly environment where we designed a
+                  website and an expense app that would be used by the company's employees. In doing so, I was able to work with new technologies and agile programming that is used by employees in the company. This internship helped me get a good view of
+                  what being a software developer is like, as well as, helping me grow in the profession." - Sam Peake, 2018 Intern
+                </p>
+              </div>
+              <div class="col-md-2 order-md-2" style="text-align:center">
+                <img
+                  src="https://res.cloudinary.com/consultwithcase/image/upload/c_crop,g_custom/c_scale,w_160/v1535719942/website/interns/sam.jpg"
+                  class="rounded-circle potrait"
+                  style="text-align:center"
+                >
+              </div>
             </div>
-          </div>
-          <div class="row" style="margin-top:100px;">
-            <div class="col-md-2" style="text-align:center">
-              <img src="https://res.cloudinary.com/consultwithcase/image/upload/v1535719942/website/interns/andrew.gif" class="rounded-circle potrait" />
+            <div class="row" style="margin-top:100px;">
+              <div class="col-md-2" style="text-align:center">
+                <img
+                  src="https://res.cloudinary.com/consultwithcase/image/upload/c_scale,w_160/v1536585437/website/interns/joe.gif"
+                  class="rounded-circle potrait"
+                >
+              </div>
+              <div class="col-md-10">
+                <p class="g-color-white">
+                  "My summer internship with Case Consulting provided me with a solid foundation for full-stack web application development that was lacking from my college education. Having the opportunity to work in an Agile environment with bleeding-edge technologies
+                  and experiencing how a software company operates has really prepared me for my career after graduation. Working in a collaborative team of other interns helped me to realize my own strengths as a developer, and we had an awesome time building
+                  an application that we are really proud of, under the guidance of the knowledgeable and approachable senior developers at Case. I would eagerly recommend an internship with Case to anyone with a desire to learn AWS, JavaScript, or web
+                  application development, without hesitation." - Joseph Thompson, 2018 Intern
+                </p>
+              </div>
             </div>
-            <div class="col-md-10">
-              <p class="g-color-white">
-                "After completing this internship, I feel as though I went from a programmer who only wanted to stay in the backend to a full stack developer who can switch between the frontend and backend depending on the needs of the team. Through the internship you
-                will learn how to be an agile developer, while working with cutting edge web technologies such as Vue, Node.js, AWS services, and Express." - Andrew Cooper, 2018 Intern
-              </p>
-            </div>
-          </div>
-          <div class="row" style="margin-top:100px;">
-            <div class="col-md-10 order-md-1 order-2">
-              <p class="g-color-white">
-                "Working at Case Consulting was a great opportunity for me to learn and experience what being a software developer is like in the real world. While I was at my internship, I was able to work in an encouraging, friendly environment where we designed a
-                website and an expense app that would be used by the company's employees. In doing so, I was able to work with new technologies and agile programming that is used by employees in the company. This internship helped me get a good view of
-                what being a software developer is like, as well as, helping me grow in the profession." - Sam Peake, 2018 Intern
-              </p>
-            </div>
-            <div class="col-md-2 order-md-2" style="text-align:center">
-              <img src="https://res.cloudinary.com/consultwithcase/image/upload/c_crop,g_custom/c_scale,w_160/v1535719942/website/interns/sam.jpg" class="rounded-circle potrait" style="text-align:center" />
-            </div>
-          </div>
-          <div class="row" style="margin-top:100px;">
-            <div class="col-md-2" style="text-align:center">
-              <img src="https://res.cloudinary.com/consultwithcase/image/upload/c_scale,w_160/v1536585437/website/interns/joe.gif" class="rounded-circle potrait" />
-            </div>
-            <div class="col-md-10">
-              <p class="g-color-white">
-                "My summer internship with Case Consulting provided me with a solid foundation for full-stack web application development that was lacking from my college education. Having the opportunity to work in an Agile environment with bleeding-edge technologies
-                and experiencing how a software company operates has really prepared me for my career after graduation. Working in a collaborative team of other interns helped me to realize my own strengths as a developer, and we had an awesome time building
-                an application that we are really proud of, under the guidance of the knowledgeable and approachable senior developers at Case. I would eagerly recommend an internship with Case to anyone with a desire to learn AWS, JavaScript, or web
-                application development, without hesitation." - Joseph Thompson, 2018 Intern
-              </p>
-            </div>
-          </div>
           </div>
         </div>
       </div>
-
-      
     </section>
     <!-- End Section Content -->
     <contact></contact>
@@ -577,12 +553,14 @@
 </template>
 
 <script>
-import Contact from '../home/Contact.vue';
+import Slider from "./Slider.vue";
+import Contact from "../home/Contact.vue";
 
 export default {
   components: {
+    "app-slider": Slider,
     contact: Contact
   }
-}
+};
 </script>
 
