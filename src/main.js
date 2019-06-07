dfabf01e6f54d43611839ea26701e6d26f660305
Megan Gilbert <megangilbert@MacBook-Pro-4.local>
2019-06-07 15:22:20 -0400

    454-get-slideshow-to-showup-when-reroute: slider works when rerouting

--- src/main.js
+++ src/main.js
@@ -14,10 +14,6 @@ const router = new VueRouter({
   scrollBehavior: (to, from, savedPosition) => {
     if (savedPosition) {
       return savedPosition;
-    } else if (to.hash) {
-      return {
-        selector: to.hash
-      };
     } else {
       return { x: 0, y: 0 };
     }
