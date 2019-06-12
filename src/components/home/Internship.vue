91ff05866dabe39957a292f4b2fa5038caaf4d61
Austin Lam <alam@consultwithcase.com>
2019-06-12 08:30:47 -0400

    clean-up-style-and-formatting-with-prettier: did some prettier formatting

--- src/components/home/Internship.vue
+++ src/components/home/Internship.vue
@@ -1,42 +1,66 @@
 <template>
   <div>
     <!-- Internship Section Content -->
-    <section id="intern" class="g-text-color-2 no-column-space intern-background">
+    <section
+      id="intern"
+      class="g-text-color-2 no-column-space intern-background"
+    >
       <div class="container-fluid">
         <div class="container text-center g-max-width-750 g-mb-20 g-pt-70">
           <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
             <h2
               class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md g-color-white"
-            >Summer Internship</h2>
+            >
+              Summer Internship
+            </h2>
           </div>
           <p class="g-color-white">
-            Summer Internships take place from May to August in the
-            Make Offices in Reston, Virginia, within the metropolitan
-            area of Washington, D.C. The interns will design and
-            implement software applications as part of a collaborative
-            team of interns and professional software developers. The
-            intern will gain experience in full stack development
-            using modern web technologies, including Amazon Web
-            Services (AWS). Interns will also learn best practices in
-            version control systems using GitHub, and will incorporate
-            Agile methodologies for self-organization and regular
-            delivery of code for production. The intern will develop
-            communication skills with customers to produce useful
-            software for the client.
+            Summer Internships take place from May to August in the Make Offices
+            in Reston, Virginia, within the metropolitan area of Washington,
+            D.C. The interns will design and implement software applications as
+            part of a collaborative team of interns and professional software
+            developers. The intern will gain experience in full stack
+            development using modern web technologies, including Amazon Web
+            Services (AWS). Interns will also learn best practices in version
+            control systems using GitHub, and will incorporate Agile
+            methodologies for self-organization and regular delivery of code for
+            production. The intern will develop communication skills with
+            customers to produce useful software for the client.
           </p>
         </div>
         <div class="container g-pb-30">
           <!-- </row> -->
-            <div class="text-center">
-              <!-- <a href="./intern.html" type="button" class="btn" role="button"> Learn More </a> -->
-              <router-link to="/intern" active-class="active" exact>
-                <button id="promoSliderLayer7" class="tp-caption NotGeneric-CallToAction rev-btn rs-parallaxlevel-0" style="z-index: 9; font-weight: bold; white-space: nowrap; outline: none; box-shadow: none; box-sizing: border-box; text-transform: uppercase; border-width: 2px;"
-                  data-x="['center','center','center','center']" data-y="['middle','middle','middle','middle']" data-hoffset="['0','0','0','0']" data-voffset="['134','134','80','65']" data-width="none" data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
-                  data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;" data-style_hover="c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);cursor:pointer;" data-transform_in="y:50px;opacity:0;s:1500;e:Power4.easeInOut;" data-transform_out="y:[175%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                  data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1250" data-splitin="none" data-splitout="none" data-actions='[{"event":"click","action":"scrollbelow","offset":"-90px"}]' data-responsive_offset="on" data-responsive="off">Learn More
-                </button>
-              </router-link>
-            </div>
+          <div class="text-center">
+            <!-- <a href="./intern.html" type="button" class="btn" role="button"> Learn More </a> -->
+            <router-link to="/intern" active-class="active" exact>
+              <button
+                id="promoSliderLayer7"
+                class="tp-caption NotGeneric-CallToAction rev-btn rs-parallaxlevel-0"
+                style="z-index: 9; font-weight: bold; white-space: nowrap; outline: none; box-shadow: none; box-sizing: border-box; text-transform: uppercase; border-width: 2px;"
+                data-x="['center','center','center','center']"
+                data-y="['middle','middle','middle','middle']"
+                data-hoffset="['0','0','0','0']"
+                data-voffset="['134','134','80','65']"
+                data-width="none"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-transform_idle="o:1;"
+                data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;"
+                data-style_hover="c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);cursor:pointer;"
+                data-transform_in="y:50px;opacity:0;s:1500;e:Power4.easeInOut;"
+                data-transform_out="y:[175%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
+                data-start="1250"
+                data-splitin="none"
+                data-splitout="none"
+                data-actions='[{"event":"click","action":"scrollbelow","offset":"-90px"}]'
+                data-responsive_offset="on"
+                data-responsive="off"
+              >
+                Learn More
+              </button>
+            </router-link>
+          </div>
           <!-- </div> -->
         </div>
       </div>
