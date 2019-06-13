c86cdfef0304ec36a7fc9c69d2c9f2dd520a764f
Sneha Kuchipudi <skuchipudi@consultwithcase.com>
2019-06-13 15:21:05 -0400

    apply-now: added apply now form and styling and css to it

--- src/components/ApplyForm/Form.vue
+++ src/components/ApplyForm/Form.vue
@@ -1,36 +1,36 @@
 <template>
   <section id="events" class="g-theme-bg-gray-light-v1 g-py-50">
-    <div class="container text-center g-max-width-750 g-mb-20 g-pt-10 g-pb-10">
-      <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
-        <h2
-          class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md"
-        >Job Application</h2>
+    <div class="applyForm container col-sm-8">
+      <div class="container text-center g-max-width-750 g-mb-20 g-pt-10 g-pb-10">
+        <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
+          <h2
+            class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md"
+          >Job Application</h2>
+        </div>
       </div>
-    </div>
 
-    <div class="container">
       <form class="form-horizontal" action="/action_page.php">
         <div class="form-group">
           <label class="control-label col-sm-2" for="name">First Name:</label>
-          <div class="col-sm-5">
+          <div class="col-sm-6">
             <input type="name" class="form-control" id="firstName" placeholder="First Name">
           </div>
         </div>
         <div class="form-group">
           <label class="control-label col-sm-2" for="name">Last Name:</label>
-          <div class="col-sm-5">
+          <div class="col-sm-6">
             <input type="name" class="form-control" id="lastName" placeholder="Last Name">
           </div>
         </div>
         <div class="form-group">
           <label class="control-label col-sm-2" for="email">Email:</label>
-          <div class="col-sm-5">
+          <div class="col-sm-6">
             <input type="email" class="form-control" id="email" placeholder="Enter email">
           </div>
         </div>
         <div class="form-group">
           <label class="control-label col-sm-2" for="email">Email:</label>
-          <div class="col-sm-5">
+          <div class="col-sm-6">
             <input type="email" class="form-control" id="email" placeholder="Enter email">
           </div>
         </div>
