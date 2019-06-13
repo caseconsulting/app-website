8a692b0ca55828a201d49a927dffb9e79a3d13c9
Sneha Kuchipudi <skuchipudi@consultwithcase.com>
2019-06-13 10:54:16 -0400

    moved env tags from json to eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
     ecmaVersion: 8,
     sourceType: 'module'
   },
   env: {
     es6: true,
     browser: true,
+    commonjs: true,
     jest: true,
+    jquery: true,
     node: true
   },
   extends: ['eslint:recommended', 'plugin:vue/essential', '@vue/prettier'],
   rules: {
     // allow console during development
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 };
