40b7d012dfdd404f16c29fb8a61e194dbec56ff0
Megan Gilbert <megangilbert@MacBook-Pro-4.local>
2019-06-13 10:33:15 -0400

    470-refactor-intern-testimonials: styling for intern testimonials on mobile

--- src/components/intern/Testimonials.vue
+++ src/components/intern/Testimonials.vue
@@ -23,11 +23,11 @@
                 <div class="col-md-2" style="text-align:center">
                   <img
                     src="https://res.cloudinary.com/consultwithcase/image/upload/v1535720007/website/interns/fahad.jpg"
-                    class="rounded-circle potrait"
+                    class="rounded-circle potrait test-image"
                   >
                 </div>
                 <div class="col-md-10">
-                  <p class="g-color-white" style="text-align:justify;">
+                  <p class="g-color-white test-par" style="text-align:justify;">
                     "Case Consulting is an awesome company that has its hands in a lot of interesting technology. At this internship, I learned what it meant to be professional in the field, I learned full-stack technologies such as NodeJS and industry standards such as
                     OAuth. We also got opportunities to explore the Amazon ecosystem and deployed an application into the real world that will be used by real people. The Case internship is a rewarding opportunity that is centered around great mentors that
                     will do everything to help you succeed."
@@ -37,9 +37,9 @@
                   </p>
                 </div>
               </div>
-              <div class="row" style="margin-top:75px;">
+              <div class="row test-row">
                 <div class="col-md-10">
-                  <p class="g-color-white" style="text-align:justify;">
+                  <p class="g-color-white test-par" style="text-align:justify;">
                     "The Case internship gave me an amazing insight into what I was getting myself into when I said I wanted to be a software developer. The company is a big family which was a major factor in my decision to come on. I learned real-world programming languages
                     and used tech that I found interesting every day I was there."
                     <template>
@@ -50,7 +50,7 @@
                 <div class="col-md-2 order-md-2" style="text-align:center">
                   <img
                     src="https://res.cloudinary.com/consultwithcase/image/upload/c_crop,g_custom/c_scale,w_160/v1535719942/website/interns/jacob.jpg"
-                    class="rounded-circle potrait"
+                    class="rounded-circle potrait test-image"
                     style="text-align:center"
                   >
                 </div>
@@ -65,11 +65,11 @@
                 <div class="col-md-2" style="text-align:center">
                   <img
                     src="https://res.cloudinary.com/consultwithcase/image/upload/v1535719942/website/interns/andrew.gif"
-                    class="rounded-circle potrait"
+                    class="rounded-circle potrait test-image"
                   >
                 </div>
                 <div class="col-md-10">
-                  <p class="g-color-white" style="text-align:justify">
+                  <p class="g-color-white test-par" style="text-align:justify">
                     "After completing this internship, I feel as though I went from a programmer who only wanted to stay in the backend to a full stack developer who can switch between the frontend and backend depending on the needs of the team. Through the internship you
                     will learn how to be an agile developer, while working with cutting edge web technologies such as Vue, Node.js, AWS services, and Express."
                     <template>
@@ -78,9 +78,9 @@
                   </p>
                 </div>
               </div>
-              <div class="row" style="margin-top:75px;">
+              <div class="row test-row">
                 <div class="col-md-10">
-                  <p class="g-color-white" style="text-align:justify">
+                  <p class="g-color-white test-par" style="text-align:justify">
                     "Working at Case Consulting was a great opportunity for me to learn and experience what being a software developer is like in the real world. While I was at my internship, I was able to work in an encouraging, friendly environment where we designed a
                     website and an expense app that would be used by the company's employees. In doing so, I was able to work with new technologies and agile programming that is used by employees in the company. This internship helped me get a good view of
                     what being a software developer is like, as well as, helping me grow in the profession."
@@ -92,7 +92,7 @@
                 <div class="col-md-2 order-md-2" style="text-align:center">
                   <img
                     src="https://res.cloudinary.com/consultwithcase/image/upload/c_crop,g_custom/c_scale,w_160/v1535719942/website/interns/sam.jpg"
-                    class="rounded-circle potrait"
+                    class="rounded-circle potrait test-image"
                     style="text-align:center"
                   >
                 </div>
@@ -108,11 +108,11 @@
                 <div class="col-md-2" style="text-align:center">
                   <img
                     src="https://res.cloudinary.com/consultwithcase/image/upload/c_scale,w_160/v1536585437/website/interns/joe.gif"
-                    class="rounded-circle potrait"
+                    class="rounded-circle potrait test-image"
                   >
                 </div>
                 <div class="col-md-10">
-                  <p class="g-color-white" style="text-align:justify">
+                  <p class="g-color-white test-par" style="text-align:justify">
                     "My summer internship with Case Consulting provided me with a solid foundation for full-stack web application development that was lacking from my college education. Having the opportunity to work in an Agile environment with bleeding-edge technologies
                     and experiencing how a software company operates has really prepared me for my career after graduation. Working in a collaborative team of other interns helped me to realize my own strengths as a developer, and we had an awesome time building
                     an application that we are really proud of, under the guidance of the knowledgeable and approachable senior developers at Case. I would eagerly recommend an internship with Case to anyone with a desire to learn AWS, JavaScript, or web
