68115f27f9fd02d544945b2e6154d10aeca69638
Sneha Kuchipudi <skuchipudi@consultwithcase.com>
2019-06-18 16:00:15 -0400

    text: refactored events and changed title text color

--- src/components/home/Events.vue
+++ src/components/home/Events.vue
@@ -3,42 +3,30 @@
   <section id="events" class="g-theme-bg-gray-light-v1 g-py-50">
     <div class="container text-center g-max-width-750 g-mb-20 g-pt-10 g-pb-10">
       <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
-        <h2
-          class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md"
-        >Events</h2>
+        <h2 class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md">Events</h2>
       </div>
     </div>
 
     <div class="col-sm-10 offset-sm-1">
       <div class="card-deck g-mx-20 g-my-20">
         <div v-for="event in events.slice(0, 4)" :key="event.id" class="card">
-          <img class="event-images" :src="event.iconLink" :alt="event.title">
+          <img class="event-images" :src="event.iconLink" :alt="event.title" />
           <div class="card-body">
             <h4 class="card-title">
-              <a
-                :href="event.driveLink"
-                style="color: red;"
-                target="_blank"
-                rel="noopener"
-              >{{event.title}}</a>
+              <a :href="event.driveLink" style="color: red;" target="_blank" rel="noopener">{{ event.title }}</a>
             </h4>
-            <p class="card-text">{{event.descrip[0]}}</p>
+            <p class="card-text">{{ event.descrip[0] }}</p>
           </div>
         </div>
 
         <div v-for="event in getArray" :key="event.id" class="collapsedEvent collapse">
           <div class="card">
-            <img class="event-images" :src="event.iconLink" :alt="event.title">
+            <img class="event-images" :src="event.iconLink" :alt="event.title" />
             <div class="card-body">
               <h4 class="card-title">
-                <a
-                  :href="event.driveLink"
-                  style="color: red;"
-                  target="_blank"
-                  rel="noopener"
-                >{{event.title}}</a>
+                <a :href="event.driveLink" style="color: red;" target="_blank" rel="noopener">{{ event.title }}</a>
               </h4>
-              <p class="card-text">{{event.descrip[0]}}</p>
+              <p class="card-text">{{ event.descrip[0] }}</p>
             </div>
           </div>
         </div>
