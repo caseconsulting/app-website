c237253d68f2d1a175bdb7146eec0d20ac0a9e70
Zak Ramsis <zramsis@Zaks-MacBook-Pro.local>
2019-06-20 11:51:39 -0400

    545-button-text-bug: fix both apply button and internship button so button can be clicked anywhere

--- src/components/ApplyButton.vue
+++ src/components/ApplyButton.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container g-pb-30 col-xl-2">
     <div class="text-center">
-      <button
-        class="tp-caption NotGeneric-CallToAction rev-btn rs-parallaxlevel-0"
+      <a
+        class="button tp-caption NotGeneric-CallToAction rev-btn rs-parallaxlevel-0"
         style="background-color: #E74C3C ;  z-index: 9; font-weight: bold; white-space: nowrap; outline: none; box-shadow: none; box-sizing: border-box; text-transform: uppercase; border-width: 2px;"
         href="/apply-form"
       >
-        <a href="/apply-form" style="color: white;">Apply Now</a>
-      </button>
+        <a style="color: white;">Apply Now</a>
+      </a>
     </div>
   </div>
 </template>
