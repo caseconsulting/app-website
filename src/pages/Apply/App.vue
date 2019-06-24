c7801ea0cb109db280ea3f6267cd3e8daee72200
Sneha Kuchipudi <skuchipudi@consultwithcase.com>
2019-06-24 17:21:32 -0400

    556-alert-user: apply form submit page

--- src/pages/Apply/App.vue
+++ src/pages/Apply/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-    <app-apply></app-apply>
+    <app-apply v-if="show"></app-apply>
+    <app-submit v-else></app-submit>
   </div>
 </template>
 
 <script>
 import Apply from '@/components/ApplyForm/Form.vue';
+import Submit from '@/components/ApplyForm/Submit.vue';
 export default {
   name: 'Apply',
   components: {
-    appApply: Apply
+    appApply: Apply,
+    appSubmit: Submit
+  },
+  data: function() {
+    return {
+      show: false
+    };
   }
 };
 </script>
