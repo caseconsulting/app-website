d3f9feba1b037464efb873a4fab59797508ee99f
Megan Gilbert <mgilbert@consultwithcase.com>
2019-06-25 09:44:40 -0400

    562-get-id-from-dynamo: use dynamo id when uploading files

--- src/components/ApplyForm/Form.vue
+++ src/components/ApplyForm/Form.vue
@@ -5,9 +5,9 @@
       <div class="applyForm container col-sm-8 col-xl-6 col-lg-8 col-xs-9">
         <div class="container text-center g-max-width-750 g-mb-20 g-pt-10 g-pb-10">
           <div class="u-heading-v2-2--bottom g-brd-primary g-mb-20">
-            <h2 class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md">
-              Job Application
-            </h2>
+            <h2
+              class="text-uppercase u-heading-v2__title g-font-weight-800 g-font-size-30 g-font-size-40--md"
+            >Job Application</h2>
           </div>
         </div>
 
@@ -17,7 +17,13 @@
               <div class="col">
                 <div class="input" :class="{ invalid: !valid.firstName }">
                   <label class="control-label" for="name">*First Name:</label>
-                  <input type="name" class="form-control" id="firstName" v-model="firstName" placeholder="First Name" />
+                  <input
+                    type="name"
+                    class="form-control"
+                    id="firstName"
+                    v-model="firstName"
+                    placeholder="First Name"
+                  >
                   <p class="invalidMsg" v-if="!valid.firstName">First name can't be blank</p>
                 </div>
               </div>
@@ -26,23 +32,35 @@
               <div class="col">
                 <div class="input" :class="{ invalid: !valid.lastName }">
                   <label class="control-label" for="name">*Last Name:</label>
-                  <input type="name" class="form-control" id="lastName" v-model="lastName" placeholder="Last Name" />
+                  <input
+                    type="name"
+                    class="form-control"
+                    id="lastName"
+                    v-model="lastName"
+                    placeholder="Last Name"
+                  >
                   <p class="invalidMsg" v-if="!valid.lastName">Last name can't be blank</p>
                 </div>
               </div>
             </div>
           </div>
-          <br />
+          <br>
           <div class="form-group">
             <div class="col-xl-12">
               <div class="input" :class="{ invalid: !valid.email }">
                 <label class="control-label" for="email">*Email:</label>
-                <input type="text" class="form-control" id="email" placeholder="Enter email" v-model="email" />
+                <input
+                  type="text"
+                  class="form-control"
+                  id="email"
+                  placeholder="Enter email"
+                  v-model="email"
+                >
                 <p class="invalidMsg" v-if="!valid.email">Please provide a valid email address.</p>
               </div>
             </div>
           </div>
-          <br />
+          <br>
           <div class="form-group">
             <div class="col-xl-12">
               <div class="input" :class="{ invalid: !valid.jobTitles }">
@@ -54,7 +72,7 @@
                   :class="{ invalid: !valid.otherJobTitle }"
                   v-if="jobTitles && jobTitles.includes('Other')"
                 >
-                  <br />
+                  <br>
                   <textarea
                     v-if="jobTitles && jobTitles.includes('Other')"
                     class="form-control"
@@ -73,7 +91,7 @@
             <div class="col-xl-12">
               <label class="control-label" for="job">How did you hear about us?:</label>
               <multiselect v-model="hearAboutUs" :options="hearOptions" :multiple="true" id="job"></multiselect>
-              <br v-if="hearAboutUs && hearAboutUs.includes('Other')" />
+              <br v-if="hearAboutUs && hearAboutUs.includes('Other')">
               <textarea
                 v-if="hearAboutUs && hearAboutUs.includes('Other')"
                 class="form-control"
@@ -83,7 +101,7 @@
               ></textarea>
             </div>
           </div>
-          <br />
+          <br>
           <div>
             <div class="col-xl-12">
               <div class="input" :class="{ invalid: !valid.resume }">
@@ -107,7 +125,7 @@
               </div>
             </div>
           </div>
-          <br />
+          <br>
           <div>
             <div class="col-xl-12">
               <label class="control-label" for="comments">Comments:</label>
@@ -120,7 +138,7 @@
               ></textarea>
             </div>
           </div>
-          <br />
+          <br>
           <div>
             <div class="col-sm-offset-2 col-sm-10">
               <button type="submit" class="btn btn-success" style="opacity: 0.8;">Submit</button>
@@ -138,7 +156,7 @@ import vueDropzone from 'vue2-dropzone';
 import { required, email } from 'vuelidate/lib/validators';
 import Header from '../home/Header.vue';
 import axios from 'axios';
-import { uuid } from 'vue-uuid';
+//import { uuid } from 'vue-uuid';
 
 // function jobTitlesNotEmpty() {
 //   return this.jobTitles != null && this.jobTitles.length > 0;
@@ -185,6 +203,7 @@ export default {
         autoProcessQueue: false,
         thumbnailWidth: '150',
         thumbnailHeight: '150',
+        key: '',
         init: function() {
           this.on('addedfile', function(file) {
             if (file.type.match(/application.pdf/)) {
@@ -212,9 +231,10 @@ export default {
         },
         acceptedFiles:
           'image/jpeg, image/png, image/gif, application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        renameFile: file => {
-          return uuid.v4() + '/' + file.name;
-        },
+        // renameFile: file => {
+        //   //return uuid.v4() + '/' + file.name;
+        //   return uuid.v4() + '/' + file.name;
+        // },
         // url: '/',
         addRemoveLinks: true,
         maxFilesize: 6,
@@ -222,8 +242,9 @@ export default {
       },
       awss3: {
         signingURL: file => {
-          console.log('filesigning: ' + file.upload.filename);
-          return `${process.env.VUE_APP_API}/upload/` + file.upload.filename;
+          //console.log('key' + this.$refs.dropzone.key);
+          console.log('filesigning: ' + this.$refs.dropzone.key + '/' + file.upload.filename);
+          return `${process.env.VUE_APP_API}/upload/` + this.$refs.dropzone.key + '/' + file.upload.filename;
           // return `https://dev.apply.consultwithcase.com/upload/` + file.upload.filename;
         },
         headers: {},
@@ -358,8 +379,10 @@ export default {
           // content upload
           const baseUrl = process.env.VUE_APP_API;
           const response = await axios.post(`${baseUrl}/apply`, data);
+          console.log(response.data.id);
           console.log(response); // eslint-disable-line no-console
-
+          this.$refs.dropzone.key = response.data.id;
+          console.log('key' + this.$refs.dropzone.key);
           // file upload
           console.log(this.$refs.dropzone.getQueuedFiles());
           this.$refs.dropzone.processQueue();
