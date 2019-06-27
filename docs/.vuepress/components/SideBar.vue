873a7b9899f4e42a34474b3cd81da5fe7570542b
Megan Gilbert <mgilbert@consultwithcase.com>
2019-06-27 15:32:43 -0400

    536-side-bar-with-tags: sidebar design complete!

--- docs/.vuepress/components/SideBar.vue
+++ docs/.vuepress/components/SideBar.vue
@@ -1,23 +1,55 @@
 <template>
   <div class="col-sm-4" style="float:center;">
     <div :v-if="displaySidebar" class="sidenav">
-      <h1>Top 10 tags</h1>
+      <h1>Top 5 tags</h1>
       <div class="row">
         <div class="number"><p>1.</p></div>
-        <a href="#">Tag1</a>
+        <div class="col">
+          <a href="#">#charity</a>
+        </div>
+        <div class="col">
+          <a style=" font-weight: bold; padding-right: 0px; float: right;" href="#">></a>
+        </div>
       </div>
 
       <div class="row">
         <div class="number"><p>2.</p></div>
-        <a href="#">Tag2</a>
+        <div class="col">
+          <a href="#">#schools</a>
+        </div>
+        <div class="col">
+          <a style=" font-weight: bold; padding-right: 0px; float: right;" href="#">></a>
+        </div>
       </div>
+
       <div class="row">
         <div class="number"><p>3.</p></div>
-        <a href="#">Tag3</a>
+        <div class="col">
+          <a href="#">#coding</a>
+        </div>
+        <div class="col">
+          <a style=" font-weight: bold; padding-right: 0px; float: right;" href="#">></a>
+        </div>
       </div>
+
       <div class="row">
         <div class="number"><p>4.</p></div>
-        <a href="#">Tag4</a>
+        <div class="col">
+          <a href="#">#bloodDrive</a>
+        </div>
+        <div class="col">
+          <a style=" font-weight: bold; padding-right: 0px; float: right;" href="#">></a>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="number"><p>5.</p></div>
+        <div class="col">
+          <a href="#">#welcomeToCase</a>
+        </div>
+        <div class="col">
+          <a style=" font-weight: bold; padding-right: 0px; float: right;" href="#">></a>
+        </div>
       </div>
     </div>
   </div>
@@ -39,7 +71,7 @@ export default {
 <style scoped>
 .sidenav {
   /* height: 100%; Full-height: remove this if you want "auto" height */
-  width: 13%;
+  /* width: 13%; */
   /* width: 200px; Set the width of the sidebar */
   position: fixed; /* Fixed Sidebar (stay in place on scroll) */
   left: 18%;
@@ -53,16 +85,18 @@ export default {
 }
 
 .sidenav .row {
-  border-bottom: 1px solid #818181;
+  border-bottom: 2px solid lightgray;
   padding-top: 10px;
 }
+
 /* The navigation menu links */
 .sidenav a {
   padding: 6px 8px 6px 16px;
   text-decoration: none;
   font-size: 16px;
-  color: #818181;
+  color: #212529;
   display: block;
+  padding-top: 12px;
 }
 
 .number {
@@ -71,7 +105,6 @@ export default {
   text-align: center;
   font-size: 16px;
   color: #818181;
-
   display: block;
 }
 
@@ -88,14 +121,15 @@ export default {
   padding-bottom: 15px;
   text-decoration: none;
   font-size: 23px;
-  color: #818181;
+  color: #212529;
   display: block;
   text-align: center;
+  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
 }
 
 .sidenav .number p {
   padding: 10px;
-  border: 1px solid #f1f1f1;
+  border: 2px solid #f1f1f1;
 }
 
 /* When you mouse over the navigation links, change their color */
@@ -103,8 +137,10 @@ export default {
   text-decoration: underline;
 }
 
-.sidenav:hover + .number {
-  background-color: blue;
+.row:hover > .number p {
+  background-color: firebrick;
+  color: white;
+  border: 2px solid lightgray;
 }
 
 /* Style page content */
@@ -123,34 +159,22 @@ export default {
   }
 }
 
-/* @media screen and (max-width: 850px) {
-  .sidenav {
-    padding-top: 20px;
-    width: 160px;
-  }
+@media screen and (max-width: 850px) {
   .sidenav a {
-    font-size: 18px;
+    font-size: 12px;
   }
 }
 
 @media screen and (max-width: 750px) {
-  .sidenav {
-    padding-top: 18px;
-    width: 120px;
-  }
   .sidenav a {
-    font-size: 18px;
+    font-size: 11px;
   }
 }
 
 @media screen and (max-width: 600px) {
-  .sidenav {
-    padding-top: 15px;
-    width: 90px;
-  }
   .sidenav a {
-    font-size: 18px;
+    font-size: 10px;
   }
-} */
+}
 </style>
 
