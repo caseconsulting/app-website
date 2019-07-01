f68750e5a6118b6a2834323cf78827bae32a6e19
Zak Ramsis <zakr2@vt.edu>
2019-07-01 11:17:08 -0400

    588-top-5-tags: sidebar now showing correct top 5 with working links plus some formatting changes

--- docs/.vuepress/components/SideBar.vue
+++ docs/.vuepress/components/SideBar.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="sidenav">
     <!-- <div class="row"> -->
-    <h1>Top 5 tags</h1>
+    <h1 style="font-weight: bold; font-family: inherit;">Top 5 tags</h1>
     <!-- </div> -->
 
-    <!-- <div>{{ tags }}</div> -->
-    <!-- <div v-for="tag in tags" class="row">
-        <div class="number"><p>1.</p></div>
-        <div class="col">
-          <a href="#"> tag </a>
+    <div v-for="(tag, index) in tags.slice(0, 5)">
+      <div class="row postTag">
+        <div class="number">
+          <p>{{ index + 1 }}.</p>
         </div>
         <div class="col">
-          <a style=" font-weight: bold; padding-right: 0px; float: right;" href="#">></a>
+          <router-link :to="{ path: `/#tag#${tag[0]}` }">
+            <div>
+              <span style="padding: 6px 90px 6px 16px;">#{{ tag[0] }}</span>
+              <span style="font-weight: bold; padding-right: 0px; float: right;">></span>
+            </div>
+          </router-link>
         </div>
-      </div> -->
+      </div>
+    </div>
 
-    <div class="row">
+    <div class="row postTag">
+      <div class="number">
+        <p>{{ 6 }}.</p>
+      </div>
+      <div class="col">
+        <router-link :to="{ path: `/#tag#tag5` }">
+          <div>
+            <span style="padding: 6px 90px 6px 16px;">#example</span>
+            <span style="font-weight: bold; padding-right: 0px; float: right;">></span>
+          </div>
+        </router-link>
+      </div>
+    </div>
+
+    <!-- <div class="row">
       <div class="number"><p>1.</p></div>
       <div class="col">
         <a href="#">#charity</a>
@@ -63,7 +82,7 @@
       <div class="col">
         <a style=" font-weight: bold; padding-right: 0px; float: right;" href="#">></a>
       </div>
-    </div>
+    </div> -->
   </div>
   <!-- Content here -->
 </template>
@@ -80,38 +99,59 @@ export default {
   methods: {
     onlyUnique(value, index, self) {
       return self.indexOf(value) === index;
+    },
+    linkTag(tag) {
+      console.log(tag);
+      location.href = this.href + tag;
     }
   },
   computed: {
     tags() {
-      let tags = [];
+      var mapZ = new Map();
       let posts = this.$site.pages;
 
-      //gets all tags from posts
       posts.forEach(post => {
-        console.log(post.frontmatter.tags);
         if (post.frontmatter.tags) {
           for (var x = 0; x < post.frontmatter.tags.length; x++) {
-            tags.push(post.frontmatter.tags[x]);
+            if (!mapZ.has(post.frontmatter.tags[x])) {
+              //if not there then set value to zero
+              mapZ.set(post.frontmatter.tags[x], 1);
+            } else {
+              mapZ.set(post.frontmatter.tags[x], mapZ.get(post.frontmatter.tags[x]) + 1);
+              //if there set value to val + 1
+            }
           }
         }
       });
+      const mapSort = [...mapZ.entries()].sort((a, b) => b[1] - a[1]);
 
-      //creates a map to sort tags by key (tag name) and value (number of appearances)
-      //value originally set to one
-      var map = new Map();
-      var unique = tags.filter(this.onlyUnique); //gets all unique tags
-      for (let value in unique) {
-        map.set(unique[value], 0);
-      }
+      // let tags = [];
 
-      //currently for debug purposes creates array with key value pairs to print
-      let array = [];
-      for (var [key, value] of map.entries()) {
-        array.push({ key, value });
-      }
+      // //gets all tags from posts
+      // posts.forEach(post => {
+      //   console.log(post.frontmatter.tags);
+      //   if (post.frontmatter.tags) {
+      //     for (var x = 0; x < post.frontmatter.tags.length; x++) {
+      //       if (tags.contains()) tags.push(post.frontmatter.tags[x]);
+      //     }
+      //   }
+      // });
 
-      return array;
+      // //creates a map to sort tags by key (tag name) and value (number of appearances)
+      // //value originally set to one
+      // var map = new Map();
+      // var unique = tags.filter(this.onlyUnique); //gets all unique tags
+      // for (let value in unique) {
+      //   map.set(unique[value], 0);
+      // }
+
+      // //currently for debug purposes creates array with key value pairs to print
+      // let array = [];
+      // for (var [key, value] of map.entries()) {
+      //   array.push({ key, value });
+      // }
+
+      return mapSort;
     }
   }
 };
@@ -134,6 +174,13 @@ export default {
   padding-bottom: 10px;
 }
 
+.postTag {
+  /* margin-top: 30px; */
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  background-color: white;
+}
+
 .sidenav .row {
   border-bottom: 2px solid lightgray;
   padding-top: 10px;
@@ -149,7 +196,7 @@ export default {
   font-size: 16px;
   color: #212529;
   display: block;
-  padding-top: 12px;
+  padding-top: 6px;
 }
 
 .number {
@@ -181,7 +228,7 @@ export default {
 }
 
 .sidenav .number p {
-  padding: 10px;
+  padding: 9px 10px 9px 14px;
   border: 2px solid lightgray;
 }
 
