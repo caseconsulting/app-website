8347041e14919ff7497ee5bc424ef5df9341fdb4
Zak Ramsis <zakr2@vt.edu>
2019-07-01 15:10:19 -0400

    552-share-image: test if correct image shows up

--- docs/.vuepress/components/BlogPost.vue
+++ docs/.vuepress/components/BlogPost.vue
@@ -3,7 +3,7 @@
     <meta property="og:url" content="location" />
     <meta property="og:type" content="website" />
     <meta property="og:title" :content="$page.frontmatter.title" />
-    <meta property="og:image" :content="$withBase($page.frontmatter.postImage)" />
+    <meta property="og:image" :content="$page.frontmatter.postImage" />
     <div class="w3-display-container">
       <div class="w3-display-container ">
         <h2 class="title">{{ $page.frontmatter.title }}</h2>
