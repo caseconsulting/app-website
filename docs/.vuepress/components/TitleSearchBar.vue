8fb524a20d13759a33469623e00fb595ed479776
Zak Ramsis <zakr2@vt.edu>
2019-07-08 14:59:14 -0400

    test-why-its-broken: remove vif

--- docs/.vuepress/components/TitleSearchBar.vue
+++ docs/.vuepress/components/TitleSearchBar.vue
@@ -20,7 +20,7 @@
           />
         </div>
         <div id="myDropdown" class="dropdown-content">
-          <a v-for="post in posts" v-if="post.frontmatter.title" @click="">{{ post.frontmatter.title }} </a>
+          <a v-for="post in posts" @click="">{{ post.frontmatter.title }} </a>
           <a @click="titleText = ''">NO RESULTS</a>
         </div>
       </div>
