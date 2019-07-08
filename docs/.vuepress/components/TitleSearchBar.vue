489a0dcf4fbb56ca46030ae3f93a40edc7428262
Zak Ramsis <zakr2@vt.edu>
2019-07-08 12:30:47 -0400

    test-why-its-broken: comment out all methods for search abr

--- docs/.vuepress/components/TitleSearchBar.vue
+++ docs/.vuepress/components/TitleSearchBar.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <!-- <head>
-      <link href="https://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css" rel="stylesheet" />
-    </head> -->
     <div style="padding-right: 20px; padding-bottom: 10px; padding-top: 0px;">
       <div>
         <div id="demo-2" class="searchContainer dropdown">
@@ -59,40 +56,40 @@ export default {
       this.$router.push(`${path}`);
     },
     showDropdown() {
-      document.getElementById('myDropdown').classList.add('show');
+      // document.getElementById('myDropdown').classList.add('show');
     },
     noShowDropdown() {
-      document.getElementById('myDropdown').classList.remove('show');
+      // document.getElementById('myDropdown').classList.remove('show');
     }
   },
   computed: {
     filterFunction() {
-      var input, filter, ul, li, a, i, div, txtValue, count;
-      input = this.titleText;
-      if (input) {
-        filter = input.toUpperCase();
-      } else {
-        filter = '';
-      }
-      div = document.getElementById('myDropdown');
-      a = div.getElementsByTagName('a');
-      count = 0;
-      for (i = 0; i < a.length; i++) {
-        txtValue = a[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue != 'NO RESULTS') {
-          a[i].style.display = '';
-          count++;
-        } else {
-          a[i].style.display = 'none';
-        }
-      }
-      // Displaying NO RESULTS
-      if (count == 0) {
-        a[a.length - 1].style.display = ''; //since the last <a></a> in the array is NO RESULTS
-      } else {
-        a[a.length - 1].style.display = 'none';
-      }
-      return input;
+      //   var input, filter, ul, li, a, i, div, txtValue, count;
+      //   input = this.titleText;
+      //   if (input) {
+      //     filter = input.toUpperCase();
+      //   } else {
+      //     filter = '';
+      //   }
+      //   div = document.getElementById('myDropdown');
+      //   a = div.getElementsByTagName('a');
+      //   count = 0;
+      //   for (i = 0; i < a.length; i++) {
+      //     txtValue = a[i].innerText;
+      //     if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue != 'NO RESULTS') {
+      //       a[i].style.display = '';
+      //       count++;
+      //     } else {
+      //       a[i].style.display = 'none';
+      //     }
+      //   }
+      //   // Displaying NO RESULTS
+      //   if (count == 0) {
+      //     a[a.length - 1].style.display = ''; //since the last <a></a> in the array is NO RESULTS
+      //   } else {
+      //     a[a.length - 1].style.display = 'none';
+      //   }
+      //   return input;
     },
     posts() {
       // let currentPage = this.page ? this.page : this.$page.path;
