7982421c563c371044887b0b1b2ab82383a273e7
Zak Ramsis <zakr2@vt.edu>
2019-07-08 14:55:34 -0400

    test-why-its-broken: empty @click

--- docs/.vuepress/components/TitleSearchBar.vue
+++ docs/.vuepress/components/TitleSearchBar.vue
@@ -20,9 +20,7 @@
           />
         </div>
         <div id="myDropdown" class="dropdown-content">
-          <a v-for="post in posts" v-if="post.frontmatter.title" @click="goToPage('a')"
-            >{{ post.frontmatter.title }}
-          </a>
+          <a v-for="post in posts" v-if="post.frontmatter.title" @click="">{{ post.frontmatter.title }} </a>
           <a @click="titleText = ''">NO RESULTS</a>
         </div>
       </div>
