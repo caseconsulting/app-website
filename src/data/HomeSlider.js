7957de14fa008a7744c2e9eba433ce2414301a7b
Chad Martin <martincc@dukes.jmu.edu>
2022-07-15 14:38:35 -0400

    optimize first image seen on website to be smaller file size

--- src/data/HomeSlider.js
+++ src/data/HomeSlider.js
@@ -1,7 +1,7 @@
 export default [
   {
     title: 'Software Oriented',
-    link: 'assets/custom/img/online/shutterstock_1150302662.jpg'
+    link: 'assets/custom/img/online/shutterstock_1150302662a.jpg'
   },
   {
     title: 'Customer Focused'
