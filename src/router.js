af2f85a3603851329df655ef05e97e8d7c7cc5e6
Chad Martin <martincc@dukes.jmu.edu>
2024-08-22 15:37:18 -0400

    Revert "remove router dynamic importing"
    This reverts commit cdbdf5a5fde40130272ddb8b9d794f8472c918e4.

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from '@/pages/Home.vue';
-import Apply from '@/pages/Apply.vue';
-import Intern from '@/pages/Intern.vue';
+const Home = () => import('@/pages/Home.vue');
+const Apply = () => import('@/pages/Apply.vue');
+const Intern = () => import('@/pages/Intern.vue');
 
 const router = createRouter({
   history: createWebHistory(),
