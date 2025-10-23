ed01db36908e7637a43859eaaead135e584f99d6
HMDekker <hdekker@consultwithcase.com>
2025-10-23 15:08:43 -0400

    switch to comp api and more dry

--- src/components/ButtonDropdown.vue
+++ src/components/ButtonDropdown.vue
@@ -2,10 +2,12 @@
   <div>
     <v-menu>
       <template v-slot:activator="{ props }">
-        <v-btn :color="color" v-bind="props"> {{ title }} </v-btn>
+        <v-btn color="primary" v-bind="props">
+          <slot></slot>
+        </v-btn>
       </template>
       <v-list>
-        <v-list-item v-for="(item, index) in items" :key="index" :value="index">
+        <v-list-item v-for="(item, index) in items" :key="index" :value="index" :to="item.link">
           <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
@@ -13,8 +15,12 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: ['items', 'title', 'color']
-};
+<script setup>
+defineProps(['items', 'title']);
 </script>
+
+<style scoped lang="scss">
+.v-list-item {
+  color: rgb(var(--v-theme-primary)) !important;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,27 +3,45 @@
     <v-app-bar :elevation="2" height="100">
       <v-app-bar-title><logo class="pl-6" /></v-app-bar-title>
       <template v-slot:append>
-        <btn-dropdown :items="items" title="about us" color="primary"></btn-dropdown>
-        <v-btn color="primary" v-bind="props"> capabilities </v-btn>
-        <v-btn color="primary" v-bind="props"> culture </v-btn>
-        <v-btn color="primary" v-bind="props"> careers </v-btn>
-        <v-btn color="secondary" variant="flat" v-bind="props" class="mr-6"> apply now </v-btn>
+        <btn-dropdown :items="dropdownItems"> temp </btn-dropdown>
+        <v-btn v-for="(item, index) in navBtns" :key="index" :value="index" color="primary" @click="handleNavigation(item.path)">
+          {{ item.title || item.path }}
+        </v-btn>
+        <v-btn color="secondary" variant="flat" class="mr-6" @click="handleNavigation('applyForm')"> apply now </v-btn>
       </template>
     </v-app-bar>
   </div>
 </template>
 
-<script>
-import ButtonDropdown from './ButtonDropdown';
-import Logo from './Logo';
+<script setup>
+import { useRouter } from 'vue-router';
+import BtnDropdown from '@/components/ButtonDropdown';
+import Logo from '@/components/Logo';
 
-export default {
-  components: {
-    'btn-dropdown': ButtonDropdown,
-    logo: Logo
-  },
-  data: () => ({
-    items: [{ title: 'Option One' }, { title: 'Option Two' }, { title: 'Option Three' }, { title: 'Option Four' }]
-  })
-};
+// |--------------------------------------------------|
+// |                                                  |
+// |                      SETUP                       |
+// |                                                  |
+// |--------------------------------------------------|
+
+const router = useRouter();
+const navBtns = [
+  { path: 'aboutUs', title: 'About Us' },
+  { path: 'capabilities' },
+  { path: 'culture' },
+  { path: 'careers' }
+];
+const dropdownItems = [{ title: 'Old Home Page', link: 'old' }];
+
+
+// |--------------------------------------------------|
+// |                                                  |
+// |                     METHODS                      |
+// |                                                  |
+// |--------------------------------------------------|
+
+
+function handleNavigation(item) {
+  router.push({ name: item });
+}
 </script>
