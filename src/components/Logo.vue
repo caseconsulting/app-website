16febce6afc3e8208ed778a25d5a36e6d602d017
HMDekker <hdekker@consultwithcase.com>
2025-10-30 11:08:30 -0400

    clean up

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -3,7 +3,3 @@
     <v-img max-width="300px" class="logo" src="/assets/custom/img/case/logo-banner.gif" alt="CASE" />
   </router-link>
 </template>
-
-<script setup>
-defineProps(['width']);
-</script>
