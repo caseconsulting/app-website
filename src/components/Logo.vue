13ad431c6e6cdf97cf0b449b0bac52ee08ea87fb
HMDekker <hdekker@consultwithcase.com>
2025-10-30 11:06:55 -0400

    router link

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
-  <a href="/">
+  <router-link to="/">
     <v-img max-width="300px" class="logo" src="/assets/custom/img/case/logo-banner.gif" alt="CASE" />
-  </a>
+  </router-link>
 </template>
 
 <script setup>
