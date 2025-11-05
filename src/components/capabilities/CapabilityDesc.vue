2125ea926e7413da4f3b436636042c0bfce5f10a
HMDekker <hdekker@consultwithcase.com>
2025-11-05 10:42:31 -0800

    start

---
+++ src/components/capabilities/CapabilityDesc.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h3 class="text-secondary mb-4"><slot name="header"></slot></h3>
+    <div class="text-secondary h5 font-emphasis"><slot name="name"></slot></div>
+  </div>
+</template>
