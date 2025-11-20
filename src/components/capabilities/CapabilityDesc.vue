8dd3b98a8fe3738a757d11fa5de11523d01c6dbe
HMDekker <hdekker@consultwithcase.com>
2025-11-19 22:45:57 -0500

    simplified capability section

--- src/components/capabilities/CapabilityDesc.vue
+++ src/components/capabilities/CapabilityDesc.vue
@@ -1,27 +1,19 @@
 <template>
   <div>
-    <v-row justify="center">
-      <h3 class="text-center text-secondary mb-4"><slot name="header"></slot></h3>
-    </v-row>
-    <v-row>
-      <v-col :order="swap ? 2 : 1" cols="6">
-        <!-- <div class="text-center h5 font-emphasis"><slot name="name"></slot></div> -->
-        <v-img class="rounded" min-width="300px" :src="url" alt="" />
+    <v-row justify="space">
+      <v-col :order="swap && !xs ? 2 : 1" align-self="center">
+        <v-img cover class="rounded" height="300px" min-width="150px" max-width="450px" :src="url" alt="" />
       </v-col>
-      <v-col :order="swap ? 1 : 2" cols="6" class="pt-10" align-self="center">
+      <v-col :order="swap && !xs ? 1 : 2" class="pt-10" align-self="center">
         <div class="h5 font-emphasis"><slot name="name"></slot></div>
         <slot name="desc"></slot>
       </v-col>
     </v-row>
-    <v-divider
-      v-if="!last"
-      thickness="6"
-      class="bg-tertiary border-opacity-75 mt-6 mb-12 mx-auto"
-      style="max-width: 125px"
-    />
   </div>
 </template>
 
 <script setup>
-defineProps(['url', 'swap', 'last']);
+import { useDisplay } from 'vuetify';
+const { xs } = useDisplay();
+defineProps(['url', 'swap']);
 </script>
