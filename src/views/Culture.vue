7b667ef3cfa74d6a4ccc9ede748dde401ca488fb
HMDekker <hdekker@consultwithcase.com>
2025-12-03 23:10:38 -0500

    culture page updates

--- src/views/Culture.vue
+++ src/views/Culture.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-6" :style="xs ? { margin: '0 1rem' } : sm ? { margin: '0 5rem' } : { margin: '0 10rem' }">
-    <h3 class="text-secondary mb-7 text-center">Case invests in you as much as you invest in your work.</h3>
+    <h3 class="text-secondary mb-7 text-center">CASE invests in you as much as you invest in your work.</h3>
     <culture-event
       v-for="(item, index) in techEvents"
       :key="index"
@@ -14,10 +14,29 @@
         <div class="mb-4">
           {{ item.desc }}
         </div>
+        <div v-if="item.secondDesc" class="mb-1">{{ item.secondDesc }}</div>
+        <bullet v-for="(bullet, index) in item.bullets" :key="index" :value="index" class="ml-5">
+          {{ bullet }}
+        </bullet>
       </template>
     </culture-event>
-    <h3 class="text-secondary mt-15 mb-7 text-center">A healthy work life balance is key.</h3>
-    <capability-desc
+    <h3 class="text-secondary mt-15 mb-7 text-center">And a healthy work life balance is key.</h3>
+    <culture-event
+      v-for="(item, index) in socialEvents"
+      :key="index"
+      :value="index"
+      :url="item.url"
+      :swap="item.swap"
+      :last="item.last"
+    >
+      <template #title>{{ item.title }}</template>
+      <template #desc>
+        <div class="mb-4">
+          {{ item.desc }}
+        </div>
+      </template>
+    </culture-event>
+    <!-- <capability-desc
       v-for="(item, index) in socialEvents"
       :key="index"
       :value="index"
@@ -31,14 +50,14 @@
           {{ item.desc }}
         </div>
       </template>
-    </capability-desc>
+    </capability-desc> -->
 
-    <h3 class="text-secondary mt-15 mb-7 text-center">A healthy work life balance is key.</h3>
+    <h3 class="text-secondary mt-15 mb-7 text-center">CASE Cares in action.</h3>
     <v-carousel style="height: 400px" class="mx-auto mt-12" hide-delimiter-background>
       <v-carousel-item v-for="(item, index) in socialEvents" :key="index" :value="index">
         <v-sheet align="center">
           <v-img :src="item.url" height="325px" width="64vw" rounded="xl" cover></v-img>
-          <div class="h5 font-emphasis pt-2">{{ item.name }}</div>
+          <div class="h5 font-emphasis pt-2">{{ item.title }}</div>
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
@@ -56,15 +75,18 @@
 
 <script setup>
 import CultureEvent from '../components/culture/CultureEvent.vue';
-import CapabilityDesc from '../components/capabilities/CapabilityDesc.vue';
+// import CapabilityDesc from '../components/capabilities/CapabilityDesc.vue';
+import Bullet from '../components/shared/Bullet.vue';
 import { useDisplay } from 'vuetify';
 
 const { xs, sm, md } = useDisplay();
 const techEvents = [
   {
     title: 'Training Resources',
-    desc: 'CASE provides a variety of training resources, from training budgets to training servers so employees can get hands on experience.',
-    url: '/assets/custom/img/events/case_work_cropped.jpg'
+    desc: 'CASE provides a variety of resources, from training budgets to training servers so employees can get hands on experience.',
+    url: '/assets/custom/img/events/case_work_cropped.jpg',
+    secondDesc: 'Our training resources include:',
+    bullets: ['Agile Coaching Sessions', 'AWS Tech Days', 'Certification Study Groups']
   },
   {
     title: 'Monthly Tech Talks',
@@ -81,20 +103,21 @@ const techEvents = [
 
 const socialEvents = [
   {
-    name: 'Annual Holiday Parties',
+    title: 'Annual Holiday Parties',
     desc: 'CASE holds summer internships whenever we can to help grow the next generation of technical talent.',
-    url: '/assets/custom/img/events/case_gala_2025.jpg'
-  },
-  {
-    name: 'Family Picnics',
-    desc: 'CASE holds summer internships whenever we can to help grow the next generation of technical talent.',
-    url: '/assets/custom/img/events/case_pinic.jpg',
+    url: '/assets/custom/img/events/case_gala_2025.jpg',
     swap: true
   },
   {
-    name: 'Case Cares',
+    title: 'Family Picnics',
     desc: 'CASE holds summer internships whenever we can to help grow the next generation of technical talent.',
-    url: '/assets/custom/img/events/2018_leesburg_race_cropped.jpg'
+    url: '/assets/custom/img/events/case_pinic.jpg'
+  },
+  {
+    title: 'Case Cares',
+    desc: 'CASE holds summer internships whenever we can to help grow the next generation of technical talent.',
+    url: '/assets/custom/img/events/2018_leesburg_race_cropped.jpg',
+    swap: true
   }
 ];
 </script>
