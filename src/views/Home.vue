ae364a0a7a95802d9134a3161a3199915733a8ee
HMDekker <hdekker@consultwithcase.com>
2025-12-08 01:01:44 -0500

    home page pic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    <v-img
+      height="30em"
+      width="100%"
+      class="mt-n1"
+      src="/assets/custom/img/online/opening_image.jpg"
+      cover
+      gradient="to top, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4)"
+    >
+      <div class="home-title text-h2 text-center text-primary">Delivering excellence<br />for mission.</div>
+    </v-img>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'Home',
-
-  created() {
-    window.onpopstate = function () {
-      history.replaceState(null, '/', '/');
-    };
-  }
-};
+<script setup>
 </script>
+
+<style scoped>
+.home-title {
+  margin-top: 2em;
+}
+</style>
