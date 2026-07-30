fafaab21cd5dba917b1e5ae8d31d2f032dd6e9a9
HMDekker <hdekker@consultwithcase.com>
2026-07-29 22:58:50 -0400

    new leadership updates

--- src/components/about-us/DetailedLeadershipCard.vue
+++ src/components/about-us/DetailedLeadershipCard.vue
@@ -5,7 +5,7 @@
         <v-col class="mr-3" cols="auto" align-self="center">
           <v-img class="rounded" height="200px" min-width="200px" :src="person.image" :alt="'Picture of ' + person.name" />
         </v-col>
-        <v-col cols="auto">
+        <v-col cols="auto" :class="person.bio ? '' : 'align-self-center'">
           <v-row class="pt-4">
             <span class="text-primary h5 font-emphasis">{{ person.name }}</span>
             <a :href="person.linkedin" target="_blank">
@@ -18,7 +18,7 @@
           <v-row>
             <div class="text-tertiary">{{ person.title }}</div>
           </v-row>
-          <v-row>{{ person.bio }}</v-row>
+          <v-row v-if="person.bio">{{ person.bio }}</v-row>
         </v-col>
       </v-row>
     </v-container>
